651c7c9f84020998c865a671e0dae490246b581e
Stephan Kugler <stephan.kugler@adesso.de>
2025-04-14 10:24:05 +0200

    delete edit btn component

--- src/app/brag-document/shared/edit-btn/edit-btn.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-edit-btn',
-  imports: [],
-  templateUrl: './edit-btn.component.html',
-})
-export class EditBtnComponent {}
