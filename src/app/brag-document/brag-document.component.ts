3e61f058177fe1d490a4aed9d25eeb30e496e31f
Stephan Kugler <stephan.kugler@adesso.de>
2025-04-15 15:32:43 +0200

    delete brag-document style document

--- src/app/brag-document/brag-document.component.ts
+++ src/app/brag-document/brag-document.component.ts
@@ -5,7 +5,6 @@ import { GoalsComponent } from './goals/goals.component';
   selector: 'app-brag-document',
   imports: [GoalsComponent],
   templateUrl: './brag-document.component.html',
-  styleUrl: './brag-document.component.css',
 })
 export class BragDocumentComponent {
   selectedYear = signal(new Date().getFullYear().toString());
