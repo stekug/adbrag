e8319b416eacf28223ae399d8136e0199faf14fa
Stephan Kugler <stephan.kugler@adesso.de>
2025-04-22 13:29:53 +0200

    add getProjectsForThisYear function to service

--- src/app/brag-document/projects/projects.component.ts
+++ src/app/brag-document/projects/projects.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { BragDocumentService } from '../brag-document.service';
+
 import { ProjectComponent } from './project/project.component';
 import { BragItemComponent } from '../../shared/brag-item/brag-item.component';
 import { SectionHeaderComponent } from '../../shared/section-header/section-header.component';
@@ -9,6 +11,18 @@ import { SectionHeaderComponent } from '../../shared/section-header/section-head
   templateUrl: './projects.component.html',
 })
 export class ProjectsComponent {
+  private bragDocumentService = inject(BragDocumentService);
+
+  selectedYear = input.required<string>();
+
+  brag = computed(() =>
+    this.bragDocumentService.getBragForYear(this.selectedYear())()
+  );
+
+  projects = computed(() =>
+    this.bragDocumentService.getProjectsForThisYear(this.selectedYear())
+  );
+
   openAddProjectForm() {
     console.log('click Add');
   }
