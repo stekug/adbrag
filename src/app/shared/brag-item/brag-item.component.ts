856b0b682440044722d9b0e918dec8d915a82653
Stephan Kugler <stephan.kugler@adesso.de>
2025-04-22 09:51:13 +0200

    create brag-item component

---
+++ src/app/shared/brag-item/brag-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brag-item',
+  imports: [],
+  templateUrl: './brag-item.component.html',
+  styleUrl: './brag-item.component.css'
+})
+export class BragItemComponent {
+
+}
