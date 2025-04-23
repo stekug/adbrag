21b7fae8acdfc1697704388bdbd722e10aeed9ee
Stephan Kugler <stephan.kugler@adesso.de>
2025-04-23 16:25:59 +0200

    feat: install angular material, implement basic date picker

--- src/app/brag-document/projects/project-form/project-form.component.ts
+++ src/app/brag-document/projects/project-form/project-form.component.ts
@@ -1,9 +1,26 @@
 import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {
+  MatDatepickerModule,
+  MatDateRangePicker,
+} from '@angular/material/datepicker';
+import { MatDateRangeInput } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 @Component({
   selector: 'app-project-form',
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDateRangePicker,
+    MatDateRangeInput,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './project-form.component.html',
 })
 export class ProjectFormComponent {
