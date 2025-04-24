078d75bf8f729ec63309386ffbc8848bc812d546
Stephan Kugler <stephan.kugler@adesso.de>
2025-04-24 15:52:09 +0200

    add confirm dialog to delete project

--- src/app/brag-document/projects/projects.component.ts
+++ src/app/brag-document/projects/projects.component.ts
@@ -6,6 +6,7 @@ import { BragItemComponent } from '../../shared/brag-item/brag-item.component';
 import { SectionHeaderComponent } from '../../shared/section-header/section-header.component';
 import { ProjectFormComponent } from './project-form/project-form.component';
 import { Project } from '../../models/brag-document.model';
+import { ConfirmDialogComponent } from '../../shared/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-projects',
@@ -14,6 +15,7 @@ import { Project } from '../../models/brag-document.model';
     ProjectFormComponent,
     BragItemComponent,
     SectionHeaderComponent,
+    ConfirmDialogComponent,
   ],
   templateUrl: './projects.component.html',
 })
@@ -23,6 +25,9 @@ export class ProjectsComponent {
   selectedYear = input.required<string>();
 
   isAddingProject = false;
+  isDeletingProject = false;
+
+  pendingDeleteProjectId = '';
 
   brag = computed(() =>
     this.bragDocumentService.getBragForYear(this.selectedYear())()
@@ -45,7 +50,21 @@ export class ProjectsComponent {
     this.bragDocumentService.saveNewProject(this.selectedYear(), newProject);
   }
 
-  handleDeleteProject(projectId: string) {
-    this.bragDocumentService.deleteProject(this.selectedYear(), projectId);
+  onCancelDeleteProject() {
+    this.isDeletingProject = false;
+  }
+
+  handleDeleteProjectRequest(projectId: string) {
+    this.isDeletingProject = true;
+    this.pendingDeleteProjectId = projectId;
+  }
+
+  handleDeleteProject() {
+    this.bragDocumentService.deleteProject(
+      this.selectedYear(),
+      this.pendingDeleteProjectId
+    );
+    this.pendingDeleteProjectId = '';
+    this.isDeletingProject = false;
   }
 }
